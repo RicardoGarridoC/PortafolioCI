00dc407dce157e74619771add0047c969fe6a1b5
Hiokiii <hioki96@hotmail.com>
2023-06-12 03:06:25 -0400

    Boton NavBar Home (Se ve porfin)

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -49,7 +49,7 @@
 <body>
 
     <!-- Navbar (ADD fixed-top a nav class para arreglar panel socios)-->
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-dark navbar-expand-lg">
         <div class="container">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
