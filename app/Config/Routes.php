f04c81054bd356132e1005cfeb4a73d5da72e043
Hiokiii <hioki96@hotmail.com>
2023-06-15 00:06:06 -0400

    Filtros Rutas Nuevas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -83,10 +83,10 @@ $routes->post('RegistrarNuevoJugador', 'RegistrarJugadorController::registrar', 
 $routes->get('RegistrarJugadorController/obtenerEquiposPorGenero/(:segment)', 'RegistrarJugadorController::obtenerEquiposPorGenero/$1', ['filter' => 'SesionDirector']);
 $routes->get('obtenerEquiposPorGenero/(:any)', 'RegistrarJugadorController::obtenerEquiposPorGenero/$1', ['filter' => 'SesionDirector']);
 $routes->get('RegistrarNuevoUsuario', 'RegistrarJugadorController::index', ['filter' => 'SesionDirector']);
-$routes->get('RegistrarNuevoEquipoTecnico', 'RegistrarEquipoTecnicoController::index');
-$routes->post('RegistrarNuevoEquipoTecnico', 'RegistrarEquipoTecnicoController::registrar');
-$routes->get('RegistrarNuevoEquipoTecnicoController/obtenerEquipos/(:segment)', 'RegistrarNuevoEquipoTecnicoController::obtenerEquipos/$1');
-$routes->post('RegistrarNuevoEquipoTecnicoController/registrarUsuario', 'RegistrarNuevoEquipoTecnicoController::registrarUsuario');
+$routes->get('RegistrarNuevoEquipoTecnico', 'RegistrarEquipoTecnicoController::index' , ['filter' => 'SesionDirector']);
+$routes->post('RegistrarNuevoEquipoTecnico', 'RegistrarEquipoTecnicoController::registrar' , ['filter' => 'SesionDirector']);
+$routes->get('RegistrarNuevoEquipoTecnicoController/obtenerEquipos/(:segment)', 'RegistrarNuevoEquipoTecnicoController::obtenerEquipos/$1' , ['filter' => 'SesionDirector']);
+$routes->post('RegistrarNuevoEquipoTecnicoController/registrarUsuario', 'RegistrarNuevoEquipoTecnicoController::registrarUsuario' , ['filter' => 'SesionDirector']);
 //Rutas Jugador
 $routes->get('InicioJugador', 'JugadorController::Dashboard', ['filter' => 'SesionJugador']);
 $routes->get('JugadoresJugador', 'JugadorController::jugadorverJugadores', ['filter' => 'SesionJugador']);
